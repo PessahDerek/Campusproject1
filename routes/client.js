712c1c7d08d861753816897801e05a08ad622506
PessahDerek <pesaderek1738@gmail.com>
2022-12-12 02:50:52 +0300

    update

--- routes/client.js
+++ routes/client.js
@@ -50,6 +50,11 @@ client.get('/gettable', async(req, res)=>{
 })
 
 client.get('/clientfoods', async(req, res)=>{
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
     let foodList = await foods.find();
     res.send(foodList);
 })
