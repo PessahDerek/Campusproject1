a3775be2bb3974b0fb94e6e1f23720fb693ae5e7
PessahDerek <pesaderek1738@gmail.com>
2022-12-12 02:41:28 +0300

    update

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ cloudinary.config({
 })
 
 // set up middleware
-app.use(cors({
-    credentials: true
-}));
+//app.use(cors({
+//    credentials: true
+//}));
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 app.use(express.urlencoded({extended: true}))
