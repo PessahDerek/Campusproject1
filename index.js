20169c75126ec5628774147b24efc12b15a536f6
PessahDerek <pesaderek1738@gmail.com>
2022-12-13 22:29:38 +0300

    'update'

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost' || 'https://roasters.netlify.app',
     credentials: true
 }));
 
